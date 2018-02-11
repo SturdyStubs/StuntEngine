d45db33b87071720cdaa01595a05f20315767188
CleverSource <stunthound@gmail.com>
2018-02-10 23:50:07 -0500

    Setup basic game window

--- StuntEngine/Program.cs
+++ StuntEngine/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
 namespace StuntEngine
 {
-    class Program
+    class Engine : GameWindow
     {
+        public Engine() :
+            base(800, 600)
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            using (Engine StuntEngine = new Engine())
+            {
+                StuntEngine.Run(60.0, 0.0);
+            }
         }
     }
 }
